891b0dc95d419c09f146baac948b6d61f781e918
Zach Oriel <Zacharyaaron15@gmail.com>
2020-01-11 05:15:23 -0800

    Major re-write of main menu & char select to work with new input system
    I love this so much more than rewired

---
+++ Assets/Scripts/Menus/CharacterSelection/CharSelectController.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CharSelectController : MonoBehaviour
+{
+    Vector2 menuInput;
+
+    enum CharacterStatus { NOT_READY, READY };
+    CharacterStatus status;
+
+    [Header("Selection Pool")]
+    public GameObject[] heads;
+    int headIndex;
+    int colorIndex;
+
+    [Space]    
+
+    [SerializeField] Image[] locatorSprites;
+
+    GameObject newHead;
+    Vector3 headPos;
+    Renderer[] childRenderers;
+    Animator animator;
+
+    bool selecting = false;
+
+    GameObject standLocation;
+
+
+    void Start()
+    {
+        headIndex = Random.Range(0, heads.Length);
+        colorIndex = Random.Range(0, ColorManager.instance.availableColors.Count);
+        animator = GetComponent<Animator>();
+        locatorSprites = GetComponentsInChildren<Image>();
+
+        AssignHead();
+        AssignColor();
+
+        standLocation = GameObject.FindGameObjectWithTag("Respawn");
+        transform.position = standLocation.transform.position;
+        standLocation.gameObject.tag = "Untagged";
+    }
+
+    void Update()
+    {
+        DetectInput();
+    }
+
+    void DetectInput()
+    {
+        Vector3 input = new Vector3(menuInput.x, menuInput.y, 0f);
+
+        // If stick moves left, a selecting isn't currently being made, and the character is not marked as ready
+        if (menuInput.x < 0f && !selecting && status == CharacterStatus.NOT_READY)
+        {
+            PreviousHead();
+        }
+
+        // If stick moves right, a selecting isn't currently being made, and the character is not marked as ready
+        if (menuInput.x > 0f && !selecting && status == CharacterStatus.NOT_READY)
+        {
+            NextHead();
+        }
+    }
+
+    void AssignHead()
+    {
+        newHead = GetComponentInChildren<Head>().gameObject;
+        headPos = newHead.transform.position;
+        Destroy(newHead.gameObject);
+
+        newHead = Instantiate(heads[headIndex], headPos, Quaternion.Inverse(transform.rotation), transform);
+        animator.SetBool("CharSelect", true);
+        animator.Play("CharSelect Idle", -1, 0);
+        newHead.GetComponent<Animator>().SetBool("CharSelect", true);
+        newHead.GetComponent<Animator>().Play("CharSelect Idle", -1, 0);
+    }
+
+    void NextHead()
+    {
+        selecting = true;
+        StartCoroutine(DelayForNextSelection());
+
+        if (headIndex >= heads.Length - 1)
+        {
+            headIndex = 0;
+        }
+        else
+        {
+            headIndex++;
+        }
+
+        AssignHead();
+    }
+
+    void PreviousHead()
+    {
+        selecting = true;
+        StartCoroutine(DelayForNextSelection());
+
+        if (headIndex <= 0)
+        {
+            headIndex = heads.Length - 1;
+        }
+        else
+        {
+            headIndex--;
+        }
+
+        AssignHead();
+    }
+
+    void AssignColor()
+    {
+        childRenderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer child in childRenderers)
+        {
+            if (child.gameObject.layer != 16)
+            {
+                child.material = ColorManager.instance.availableColors[colorIndex];
+            }
+        }
+
+        locatorSprites = GetComponentsInChildren<Image>();
+        foreach (Image locator in locatorSprites)
+        {
+            Color emissColor = ColorManager.instance.availableColors[colorIndex].GetColor("_EmissionColor");
+            locator.color = emissColor;
+        }
+
+        for (int i = 0; i < ColorManager.instance.takenColors.Count; i++)
+        {
+            if (ColorManager.instance.takenColors[i] == null)
+            {
+                ColorManager.instance.takenColors[i] = ColorManager.instance.availableColors[colorIndex];
+                break;
+            }
+        }
+
+        ColorManager.instance.availableColors.RemoveAt(colorIndex);
+    }
+
+    IEnumerator DelayForNextSelection()
+    {
+        yield return new WaitForSeconds(0.2f);
+        selecting = false;
+        yield return null;
+    }
+
+    void OnSwapHead(InputValue value)
+    {
+        menuInput = value.Get<Vector2>();
+    }
+
+    void OnReadyUp()
+    {
+        Debug.Log("Player ready!");
+        status = CharacterStatus.READY;
+        ReadyStatus.instance.playersReady++;
+        ReadyStatus.instance.CheckIfAllPlayersReady();
+    }
+
+    void OnUnReady()
+    {
+        Debug.Log("Player not ready!");
+        status = CharacterStatus.NOT_READY;
+        ReadyStatus.instance.playersReady--;
+        ReadyStatus.instance.CheckIfAllPlayersReady();
+    }
+
+    void OnGoBack()
+    {
+        Debug.LogError("Zach hasn't implementing going back to the main menu yet");
+    }
+}
---
+++ Assets/Scripts/Menus/CharacterSelection/ReadyStatus.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class ReadyStatus : MonoBehaviour
+{
+    public static ReadyStatus instance;
+
+    public string levelSelect;
+    public Text countdownText;
+    Coroutine countdown;
+
+    [SerializeField] float time = 5f;
+    bool countingDown;
+
+    CharSelectController[] players;
+    [HideInInspector] public int playersReady = 0;
+
+
+    #region Singleton
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    #endregion
+
+    void Start()
+    {
+        countdownText.enabled = false;
+    }
+
+    public void CheckIfAllPlayersReady()
+    {
+        players = FindObjectsOfType<CharSelectController>();
+
+        if (playersReady == players.Length && players.Length != 0)
+        {
+            StartCountdown();
+        }
+        else
+        {
+            StopCountdown();
+        }
+    }
+
+    void StartCountdown()
+    {
+        time = 4f;
+        countingDown = true;
+        countdown = StartCoroutine(CountdownToGame());
+    }
+
+    void StopCountdown()
+    {
+        countingDown = false;
+        countdown = null;
+        countdownText.enabled = false;
+    }
+
+    IEnumerator CountdownToGame()
+    {
+        countdownText.enabled = true;
+
+        while (countingDown)
+        {
+            if (time > 0f)
+            {
+                time -= 1f;
+                countdownText.text = "Starting Game In: " + Mathf.RoundToInt(time).ToString();
+
+                yield return new WaitForSeconds(1f);
+            }
+            else
+            {
+                ContinueToGame();
+                break;
+            }
+        }
+    }
+
+    void ContinueToGame()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            DontDestroyOnLoad(players[i]);
+
+            Animator[] animators = players[i].GetComponentsInChildren<Animator>();
+            for (int j = 0; j < animators.Length; j++)
+            {
+                animators[j].Play("CharSelect Idle", -1, 0.8f);
+                animators[j].SetBool("CharSelect", false);
+            }
+
+            players[i].GetComponent<PlayerController>().enabled = true;
+            Destroy(players[i].GetComponent<CharSelectController>());
+            Destroy(players[i].GetComponent<PlayerInput>());
+        }
+
+        SceneFader.instance.FadeTo(levelSelect);
+    }
+}
