7e2917039eacfb44a3d814591fef70cf303e7c2e
Shea McAuley <thespookter5123@gmail.com>
2020-02-23 19:20:13 -0800

    CharacterSelection Initialized

---
+++ Assets/CharacterSelect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterSelect : MonoBehaviour
+{
+    PlayerInputManager inputManager;
+    PlayerInput input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputManager = GetComponent<PlayerInputManager>();
+
+        inputManager.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
